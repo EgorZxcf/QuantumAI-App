439ce5cca3f94e33bbdeac6c727f643ca1778e9c
Egor <uduakegor@gmail.com>
2026-05-27 10:59:31 +0300

    ai memory added

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from openai import OpenAI
 import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///memory.db"
+
+db = SQLAlchemy(app)
+class Message(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    role = db.Column(db.String(20))
+
+    content = db.Column(db.Text)
 
 # Разрешаем запросы от приложения
 CORS(app)
@@ -31,6 +42,14 @@ def chat():
             }), 400
 
         message = data.get("message")
+       db.session.add(
+    Message(
+        role="user",
+        content=message
+    )
+)
+
+db.session.commit()
 
         if not message:
             return jsonify({
@@ -39,15 +58,22 @@ def chat():
 
         response = client.chat.completions.create(
             model="openai/gpt-4o-mini",
-            messages=[
-                {
-                    "role": "user",
-                    "content": message
-                }
-            ]
-        )
+        messages=[
+        history = Message.query.order_by(
+    Message.id.desc()
+).limit(10).all()
 
-        ai_response = response.choices[0].message.content
+history.reverse()
+
+messages = []
+
+for msg in history:
+
+    messages.append({
+        "role": msg.role,
+        "content": msg.content
+    })
+answer = response.choices[0].message.content
 
         return jsonify({
             "response": ai_response
@@ -60,4 +86,8 @@ def chat():
         }), 500
 
 if __name__ == "__main__":
+   with app.app_context():
+    db.create_all()
     app.run(host="0.0.0.0", port=5000)
+
+
